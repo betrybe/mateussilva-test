4fd546d33a15b473ef0456295cc4a9f5842d44d7
Mateus Silva <thi.hero2012@gmail.com>
2021-09-07 15:11:47 -0300

    Desenvolvimento Completo
    Implementado lógicas de importação e de listagem das listas, incluindo a lógica para pegar os argumentos passados por linha de comanda e utilizá-los para gerar o relatório.

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,27 @@
+import sys
+import xml.etree.ElementTree as ET
+
+from os.path import dirname, join, abspath
+
+sys.path.insert(0, abspath(join(dirname(__file__), "..")))
+
+from .importer import Importer
+
+
+class XmlImporter(Importer):
+    def import_data(self, caminho_arquivo):
+        try:
+            with open(caminho_arquivo) as arquivo:
+                if caminho_arquivo[-4:] == ".xml":
+                    report_data = []
+                    tree = ET.parse(arquivo)
+                    root = tree.getroot()
+                    for child in root:
+                        report_item = {}
+                        for element in child:
+                            report_item[element.tag] = element.text
+                        report_data.append(report_item)
+                    return report_data
+                raise ValueError("Arquivo inválido")
+        except ValueError as ex:
+            print(ex.mensagem)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,46 @@
+import sys
+import getopt
+from inventory.inventory_refactor import InventoryRefactor
+from importer.csv_importer import CsvImporter
+from importer.json_importer import JsonImporter
+from importer.xml_importer import XmlImporter
+from reports.simple_report import SimpleReport
+from reports.complete_report import CompleteReport
+
+import json
+
+
 def main():
-    pass
+    args = sys.argv
+    try:
+        # Verificar argumentos passados por linha de comando
+        if len(args) != 3:
+            raise getopt.error("Verifique os argumentos")
+        caminho_arquivo, tipo_relatorio = args[1:]
+        # Verificar qual classe utilizar para cada tipo de arquivo
+        if caminho_arquivo[-4:] == ".csv":
+            instance = InventoryRefactor(CsvImporter)
+        elif caminho_arquivo[-5:] == ".json":
+            instance = InventoryRefactor(JsonImporter)
+        elif caminho_arquivo[-4:] == ".xml":
+            instance = InventoryRefactor(XmlImporter)
+
+        # Gerar a lista de dicionários
+        report_data = instance.import_data(caminho_arquivo)
+
+        # Gerar relatórios
+        if tipo_relatorio == "simples":
+            relatorio = SimpleReport(report_data).generate()
+        elif tipo_relatorio == "completo":
+            relatorio = CompleteReport(report_data).generate()
+        else:
+            raise getopt.error("Verifique os argumentos")
+        # Mostrar relatório
+        print(relatorio)
+
+    except getopt.error as err:
+        print(str(err), file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
